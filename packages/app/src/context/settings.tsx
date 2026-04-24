a34805d06709c9fa730d37658b5e54341f14c12c
yqmaphy <yqma@pku.edu.cn>
2026-04-24 23:04:28 +0800

    Revert "fix: restore branchesTab default to false"
    This reverts commit 4041b614412d8c3048b4479a68a0ebcbc1e86850.

--- packages/app/src/context/settings.tsx
+++ packages/app/src/context/settings.tsx
@@ -53,7 +53,7 @@ const defaultSettings: Settings = {
     releaseNotes: true,
     followup: "steer",
     reviewBatch: 10,
-    branchesTab: false,
+    branchesTab: true,
     branchGraphFontSize: "lg",
     branchGraphRowDensity: "compact",
     branchGraphOrderMode: "time",
